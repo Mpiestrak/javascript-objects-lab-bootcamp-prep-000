1ed71657a9f3beb6c03fdae51ab636aacfcac564
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-29 18:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function deleteFromObjectByKey(object, key){
   return recipes
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-  var recipes2 = delete object.key
+function destructivelyDeleteFromObjectByKey(recipes, breakfast){
   return recipes
 }
