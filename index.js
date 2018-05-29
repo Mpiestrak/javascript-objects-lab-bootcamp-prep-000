229e4bbbbd9cb689a2409fff3e1064bc2e9e30fa
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-29 17:59:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {key: "value"};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+} 
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var recipes2 = Object.assign({}, object)
+  delete recipes2.key
+  return recipes
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete recipes.key
+  return recipes
+}
